2f7c9bb33c90fbc0144578e436c945009b1283f4
maweiyan <1107201045@qq.com>
2023-06-15 22:47:41 +0800

    crud

--- src/views/admin/system/user/index.vue
+++ src/views/admin/system/user/index.vue
@@ -1,11 +1,55 @@
 <script setup lang="ts">
+  import { reactive, ref } from 'vue'
   import UserApi from '@/api/admin/system/user'
+  import Option from '@/option/admin/system/user'
 
+  let form = reactive({
+
+  })
+  let page = reactive({})
+  let option = reactive(Option())
+  let data = reactive([
+    {
+      name: 'zs',
+      value: '1',
+      code: '1001',
+      node: 'remark'
+    },
+    {
+      name: 'ls',
+      value: '1',
+      code: '1',
+      node: 'remark'
+    }
+  ])
+  let loading = ref(false)
   let userApi: UserApi = UserApi.getInstance()
+  let param = reactive({})
+  let save = (row, done, loading) => {}
+  let deleteByIds = row => {}
+  let updateById = (row, index, done, loading) => {}
+  let getListByPageAndQuery = (params, done) => {
+    if (done) {
+      done()
+    }
+  }
 </script>
-
 <template>
-  <el-button type="success">按钮</el-button>
+  <avue-crud
+    ref="crud"
+    v-model="form"
+    v-model:page="page"
+    :option="option"
+    :data="data"
+    :table-loading="loading"
+    @row-save="save"
+    @row-del="deleteByIds"
+    @row-update="updateById"
+    @on-load="getListByPageAndQuery"
+    @search-change="getListByPageAndQuery"
+    @search-reset="getListByPageAndQuery"
+    @refresh-change="getListByPageAndQuery"
+  />
 </template>
 
 <style scoped></style>
